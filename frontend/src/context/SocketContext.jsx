dd410098ad6656ce71315815c66f99d7a9ca27b1
PrashantB-307 <pb28.rkt@gmail.com>
2024-04-13 17:33:31 +0530

    :camera_flash::camera_flash: Socket Context :camera_flash::camera_flash:

---
+++ frontend/src/context/SocketContext.jsx
@@ -0,0 +1,47 @@
+import { createContext, useState, useEffect, useContext } from "react";
+import { useAuthContext } from "./AuthContext";
+import io from "socket.io-client";
+
+const SocketContext = createContext();
+
+export const useSocketContext = () => {
+    return useContext(SocketContext);
+};
+
+
+export const SocketContextProvider = ({ children }) => {
+    const [socket, setSocket] = useState(null);
+    const [onlineUsers, setOnlineUsers] = useState([]);
+    const { authUser } = useAuthContext();
+
+    useEffect(() => {
+        if (authUser) {
+            // https://localhost:5000
+            const newSocket = io("https://chat-app-biu2.onrender.com", {
+                query: {
+                    userId: authUser._id,
+                },
+            });
+
+            setSocket(newSocket);
+
+            // socket.on() is used to listen to the events. can be used both on client and server side
+            newSocket.on("getOnlineUsers", (users) => {
+                setOnlineUsers(users);
+            });
+
+            return () => {
+                if (newSocket) {
+                    newSocket.close();
+                }
+            };
+        } else {
+            if (socket) {
+                socket.close();
+                setSocket(null);
+            }
+        }
+    }, [authUser]);
+
+    return <SocketContext.Provider value={{ socket, onlineUsers }}>{children}</SocketContext.Provider>;
+};
